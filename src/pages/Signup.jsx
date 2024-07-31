f9d62f61316f22a1f1e520e687fda43b112dd99d
asad-ullah-turab <asadullahturab47@gmail.com>
2024-07-31 14:08:23 +0500

    chore: Add link to homepage in signup page

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,10 +1,13 @@
+import { Link } from "react-router-dom";
 import { Footer, Logo } from "../components";
 
 export default function Signup() {
   return (
     <div className="h-screen w-screen md-2:px-10 px-5 py-5">
       <div className="">
-        <Logo width="200px" className="" />
+        <Link to="/">
+          <Logo width="200px" className="" />
+        </Link>
       </div>
       <Footer />
     </div>
