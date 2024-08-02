baa71c5806b5482defcf87727406220a321dfa44
asad-ullah-turab <asadullahturab47@gmail.com>
2024-08-02 14:04:00 +0500

    setting up gh-pages

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { ghPages } from "vite-plugin-gh-pages";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    host: "0.0.0.0",
-    port: 5173,
-  },
+  plugins: [react(), ghPages()],
+  base: "/blog/",
 });
